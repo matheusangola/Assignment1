d98ac8ceac9640d2b87e599c55c081a7c92816db
Matheus <Matheus@DESKTOP-S044LP0>
2024-09-24 19:37:06 -0700

    Add project files.

---
+++ Assignment1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment1
+{
+    // TODO Add supporting classes here
+
+    public class Program
+    {
+        public static string ProcessCommand(string input)
+        {
+            try
+            {
+                // TODO Evaluate the expression and return the result
+                return "";
+            }
+            catch (Exception e)
+            {
+                return "Error evaluating expression: " + e;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string input;
+            while ((input = Console.ReadLine()) != "exit")
+            {
+                Console.WriteLine(ProcessCommand(input));
+            }
+        }
+    }
+}
