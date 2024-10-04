1a5ecb0c3422867b4d9d94cc3b3a234bcf218504
Matheus Alves <matheusangola97@gmail.com>
2024-10-03 20:20:03 -0700

    Implemented ProcessCommand

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -22,13 +22,110 @@ namespace Assignment1
     }
     public class InfixToPostfix
     {
-        
+        public static List<string> ConvertToPostfix(string input)
+        {
+            Stack<char> operators = new Stack<char>();
+            List<string> output = new List<string>();
+            int i = 0;
+
+            while (i < input.Length)
+            {
+                char currentChar = input[i];
+                if (Char.IsDigit(currentChar) || (currentChar == '-' &&
+                    (i == 0 || input[i - 1] == '(' || OperatorClass.IsOperator(input[i - 1]))))
+                {
+                    string number = "";
+                    if (currentChar == '-')
+                    {
+                        number += currentChar;
+                        i++;
+                        if (i < input.Length)
+                            currentChar = input[i];
+                    }
+
+                    while (i < input.Length && (Char.IsDigit(currentChar) || currentChar == '.'))
+                    {
+                        number += currentChar;
+                        i++;
+                        if (i < input.Length)
+                            currentChar = input[i];
+                    }
+
+                    output.Add(number);
+                    continue;
+                }
+                else if (currentChar == '(')
+                {
+                    operators.Push(currentChar);
+                }
+                else if (currentChar == ')')
+                {
+                    while (operators.Peek() != '(')
+                    {
+                        output.Add(operators.Pop().ToString());
+                    }
+                    operators.Pop();
+                }
+                else if (OperatorClass.IsOperator(currentChar))
+                {
+                    while (operators.Count > 0 && OperatorClass.OperatorOrder(operators.Peek()) >= OperatorClass.OperatorOrder(currentChar))
+                    {
+                        output.Add(operators.Pop().ToString());
+                    }
+                    operators.Push(currentChar);
+                }
+
+                i++;
+            }
+            while (operators.Count > 0)
+            {
+                output.Add(operators.Pop().ToString());
+            }
+
+            return output;
+        }
     }
 
     
     public class Calc
     {
-        
+        public static double CalcPostfix(List<string> tokens)
+        {
+            Stack<double> stack = new Stack<double>();
+
+            foreach (var token in tokens)
+            {
+                if (double.TryParse(token, out double num))
+                {
+                    stack.Push(num);
+                }
+                else if (token == "+" || token == "-" || token == "*" || token == "/")
+                {
+                    double b = stack.Pop();
+                    double a = stack.Pop();
+                    double result = 0;
+
+                    switch (token)
+                    {
+                        case "+":
+                            result = a + b;
+                            break;
+                        case "-":
+                            result = a - b;
+                            break;
+                        case "*":
+                            result = a * b;
+                            break;
+                        case "/":
+                            result = a / b;
+                            break;
+                    }
+                    stack.Push(result);
+                }
+            }
+
+            return stack.Pop();
+        }
     }
 
 
@@ -41,9 +138,14 @@ namespace Assignment1
             {
                 input = input.Replace(" ", "");
 
-                String result = "0.0";
+                List<string> postFixValue = InfixToPostfix.ConvertToPostfix(input);
 
-                return result;
+                //Console.WriteLine("RPN Expression: " + string.Join(" ", postFixValue));
+
+                double result = Calc.CalcPostfix(postFixValue);
+
+                return result.ToString();
+
             }
             catch (Exception e)
             {
@@ -58,7 +160,6 @@ namespace Assignment1
             while ((input = Console.ReadLine()) != "exit")
             {
                 Console.WriteLine(ProcessCommand(input));
-            }
             }
         }
     }
